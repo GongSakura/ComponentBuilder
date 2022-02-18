f93de2d6703c4f1580c1082dc89d492a869af051
Ryan Yang <ryanyang@lilith.com>
2022-02-18 19:05:08 +0800

    feat: scale and resize stage, resize item

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,72 @@
 
-import React, { ReactNode,Component } from 'react'
-import {InteractElement,InteractProps} from './components/interactElement'
+import React, { ReactNode,Component, ChangeEvent } from 'react'
+import {InteractElement,InteractItemProps} from './components/interactElement'
 import './App.less'
 
 interface AppState{
   interactElements:Array<ReactNode>
-}
+  stageScale:number
+  stageWidth:number
+  stageHeight:number
+  stagePosition:{
+    x:number
+    y:number
+  }
+  }
 type AppProp = Record<string,never>
 
 class App extends Component<AppProp,AppState>{
   interactElementNumber=0
+  interactBoard!:HTMLElement
+  interactStage!:HTMLElement
+  scaleSmallStep=0.025
+  scaleLargeStep=0.1
+  scaleUpperBound=3
+  scaleLowerBound=0.5
+  mouseInBoard=false
+  grabInBoard=false
+  moveInBoard=false
+  mousePosition={
+    x:0,
+    y:0,
+  }
+
   constructor(props:AppProp){
     super(props)
     this.state={
-      interactElements:[]
+      interactElements:[],
+      stageScale:1,
+      stageWidth:100,
+      stageHeight:100,
+      stagePosition:{
+        x:0,
+        y:0
+      }
     }
     this.addInteractElement=this.addInteractElement.bind(this)
-    
+    this.handleScaleStageEvent=this.handleScaleStageEvent.bind(this)
+    this.handleResizeStageEvent=this.handleResizeStageEvent.bind(this)
+    this.handleKeyDownEvent=this.handleKeyDownEvent.bind(this)
+    this.handleKeyUpEvent=this.handleKeyUpEvent.bind(this)
+    this.handleMouseEvent=this.handleMouseEvent.bind(this)
   }
 
   addInteractElement(event: React.MouseEvent):void{
     event.preventDefault()
-    const interactProps:InteractProps={
+    const interactProps:InteractItemProps={
       draggable:true,
       dropzone:false,
       resizable:true,
+      stageScale:this.state.stageScale
       }
       const updateInteractElements:Array<ReactNode> = [...this.state.interactElements]
-      updateInteractElements.push(<InteractElement  key={this.interactElementNumber} {...interactProps}><div>test-dom</div></InteractElement>)
+      updateInteractElements.push(<InteractElement key={this.interactElementNumber} {...interactProps}><div>test-dom</div></InteractElement>)
       this.setState({
         interactElements:updateInteractElements
       })   
       this.interactElementNumber++
   }
-  addSelected(event:MouseEvent){
+  handleSelectItemEvent(event:MouseEvent){
   
     document.querySelectorAll('.interact-item.selected').forEach(e=>e.classList.remove('selected'))
     const target:HTMLElement= event.target as HTMLElement
@@ -41,25 +74,166 @@ class App extends Component<AppProp,AppState>{
       target.classList.add('selected')
     }
   }
+  handleScaleStageEvent(event:WheelEvent){
+    console.log(event)
+    console.log()
+    if(event.metaKey){
+      event.preventDefault()
+      const oldScaleRatio = this.state.stageScale
+      let newScaleRatio = oldScaleRatio
+      if(event.deltaY>0 && oldScaleRatio>this.scaleLowerBound){
+          // scale down
+          oldScaleRatio<1?newScaleRatio-=this.scaleSmallStep:newScaleRatio-=this.scaleLargeStep
+      }else if(event.deltaY<0 && oldScaleRatio<this.scaleUpperBound){
+        oldScaleRatio<1?newScaleRatio+=this.scaleSmallStep:newScaleRatio+=this.scaleLargeStep
+      }
+      if(newScaleRatio!==oldScaleRatio){
+        this.setState({
+          stageScale:newScaleRatio
+        })
+
+          const rect = this.interactBoard.getBoundingClientRect()
+          const pointer={
+            x:event.clientX-rect.x,
+            y:event.clientY-rect.y
+          }
+          const mousePointTo={
+            x:(pointer.x-this.state.stagePosition.x)/oldScaleRatio,
+            y:(pointer.y-this.state.stagePosition.y)/oldScaleRatio
+        }
+
+        this.setState({
+          stagePosition:{
+            x:pointer.x-mousePointTo.x*newScaleRatio,
+            y:pointer.y-mousePointTo.y*newScaleRatio,
+          }
+        })
+      }
+      
+    }
+  }
+  handleResizeStageEvent(event:ChangeEvent){
+    const target = event.currentTarget as HTMLInputElement
+    if(target.classList.contains('page-width')){
+      const value = parseInt(target.value) || 0
+      this.setState({stageWidth:value})
+    }else if(target.classList.contains('page-height')){
+      const value = parseInt(target.value) || 0
+      this.setState({stageHeight:value})
+    }
+  }
+  handleMouseEvent(event:MouseEvent){
+
+    switch (event.type){
+      case 'mouseenter':
+        this.mouseInBoard=true
+        break
+      case 'mouseleave':
+        this.mouseInBoard=false
+        break
+      case 'mousemove':
+        if(this.grabInBoard && this.moveInBoard){
+          const n_x = this.state.stagePosition.x +event.clientX-this.mousePosition.x
+          const n_y = this.state.stagePosition.y +event.clientY-this.mousePosition.y
+          this.setState({
+            stagePosition:{
+              x:n_x,
+              y:n_y
+            }
+          })
+          this.mousePosition.x=event.clientX
+          this.mousePosition.y=event.clientY
+      }
+        break
+      case 'mousedown':
+        if(this.grabInBoard){
+          this.moveInBoard=true
+          this.mousePosition.x=event.clientX
+          this.mousePosition.y=event.clientY
+        }
+        break
+      case 'mouseup': 
+        this.moveInBoard=false
+        break
+
+    }
+    
+  }
+  handleKeyDownEvent(event:KeyboardEvent){
+    event.preventDefault()
+    if(event.code==='Space' && this.mouseInBoard){
+      this.interactBoard.style.cursor='grab'
+      this.grabInBoard=true
+    }
+  }
+  handleKeyUpEvent(event:KeyboardEvent){
+    event.preventDefault()
+    if(event.code==='Space' ){
+      this.interactBoard.style.cursor=''
+      this.grabInBoard=false
+    }
+    
+  }
+
   componentDidMount(){
-    window.addEventListener('click',this.addSelected)
+    this.interactBoard=document.querySelector('#interact-board') as HTMLElement
+    this.interactStage=document.querySelector('#interact-stage') as HTMLElement
+
+    this.interactBoard?.addEventListener('click',this.handleSelectItemEvent)
+    this.interactBoard?.addEventListener('mousemove',this.handleMouseEvent)
+    this.interactBoard?.addEventListener('mousedown',this.handleMouseEvent)
+    this.interactBoard?.addEventListener('mouseup',this.handleMouseEvent)
+    this.interactBoard?.addEventListener('mouseenter',this.handleMouseEvent)
+    this.interactBoard?.addEventListener('mouseleave',this.handleMouseEvent)
+
+    document.addEventListener('keydown',this.handleKeyDownEvent)
+    document.addEventListener('keyup',this.handleKeyUpEvent)
+    this.interactStage?.addEventListener('wheel',this.handleScaleStageEvent)
   }
   componentWillUnmount(){
-    window.removeEventListener('click',this.addSelected)
+    this.interactBoard?.removeEventListener('click',this.handleSelectItemEvent)
+    this.interactBoard?.removeEventListener('mousemove',this.handleMouseEvent)
+    this.interactBoard?.removeEventListener('mousedown',this.handleMouseEvent)
+    this.interactBoard?.removeEventListener('mouseup',this.handleMouseEvent)
+    this.interactBoard?.removeEventListener('mouseenter',this.handleMouseEvent)
+    this.interactBoard?.removeEventListener('mouseleave',this.handleMouseEvent)
+
+    document.removeEventListener('keydown',this.handleKeyDownEvent)
+    document.removeEventListener('keyup',this.handleKeyUpEvent)
+
+    this.interactStage?.removeEventListener('wheel',this.handleScaleStageEvent)
   }
   render(): ReactNode {
       return <>
-        <div className='interact-toolbar'>
+        <div id='interact-toolbar'>
           <button onClick={this.addInteractElement}>Add</button>
+          <div id='interact-controller'>
+            <span>Width</span>
+            <input className='page-width' type="text" value={this.state.stageWidth} onChange={this.handleResizeStageEvent}/>
+            <span>Height</span>
+            <input className='page-height' type="text"  value={this.state.stageHeight} onChange={this.handleResizeStageEvent}/>
+            <span>SclaeRatio: {this.state.stageScale.toFixed(2)}</span>
+          </div>
         </div>
-        <div className='interact-board'>
-          <div className='interact-container'>
+        <div id='interact-board'>
+          <div id='interact-stage' style={
+            {transform:`scale(${this.state.stageScale})`,
+            width:this.state.stageWidth+'%',
+            height:this.state.stageHeight+'%',
+            left:this.state.stagePosition.x+'px',
+            top:this.state.stagePosition.y+'px'}}> 
             {this.state.interactElements}
           </div>
         </div>
       </>
   }
 }
+{/* <div id='interact-stage' style={
+            {transform:`scale(${this.state.stageScale}) translateX(${this.state.stagePosition.x}px) translateY(${this.state.stagePosition.y}px)`,
+            width:this.state.stageWidth+'%',
+            height:this.state.stageHeight+'%'}}>
+            {this.state.interactElements}
+          </div> */}
 
 
 export default App
--- src/components/interactElement/index.tsx
+++ src/components/interactElement/index.tsx
@@ -2,108 +2,175 @@ import './index.less'
 import React, { cloneElement, ReactElement } from 'react'
 import interact from 'interactjs'
 import type { DraggableOptions, DropzoneOptions, ResizableOptions, Interactable, DragEvent, ResizeEvent,PointerEventType } from '@interactjs/types/index'
+import modifiers from '@interactjs/modifiers/plugin'
 
-export interface InteractProps {
+export interface InteractItemProps {
     draggable: boolean
     draggableOptions?: DraggableOptions
     dropzone: boolean
     dropzoneOptions?: DropzoneOptions
     resizable: boolean
     resizableOptions?: ResizableOptions
+    stageScale:number
+}
+interface InteractItemState{
+    itemPosition:{
+        x:number
+        y:number
+    },
+    itemSize:{
+        width:number
+        height:number
+    }
 }
 
 
 
-export class InteractElement extends React.Component<InteractProps>{
+export class InteractElement extends React.Component<InteractItemProps,InteractItemState>{
     node?: HTMLElement
     interact?: Interactable
     draggableOptions: DraggableOptions
     resizableOptions: ResizableOptions
-    position: {
-        x: number
-        y: number
-        translateX: number
-        translateY: number
+    tempItemPosition:{
+        x:number
+        y:number
+     }
+     tempItemSize?:{
+         width:number
+         height:number
+     }
+    mousePosition={
+        x:0,
+        y:0
     }
-    constructor(props: InteractProps) {
+
+    constructor(props: InteractItemProps) {
         super(props)
-        this.position = { x: 0, y: 0, translateX: 0, translateY: 0 }
+        this.state={
+            itemPosition:{
+                x:0,
+                y:0
+            },
+            itemSize:{
+                width:50,
+                height:100,
+            }
+        }
+        this.tempItemPosition={
+            x:this.state.itemPosition.x,
+            y:this.state.itemPosition.y
+        }
+  
         this.draggableOptions = {
             onstart:(event: DragEvent)=>{
-          
+                this.mousePosition.x=event.clientX
+                this.mousePosition.y=event.clientY
                 const target:HTMLElement = event.target as HTMLElement
-                if(!target.classList.contains('selected')){
-                    target.classList.add('selected')
+                if(!target.classList.contains('dragging')){
+                    target.classList.add('dragging')
                 }
             },
             onmove: (event: DragEvent) => {
-                this.position.translateX += event.dx
-                this.position.translateY += event.dy
-                const target = event.target as HTMLElement
+                console.log(event)
+                
+                const n_x = this.tempItemPosition.x + event.clientX-this.mousePosition.x
+                const n_y = this.tempItemPosition.y + event.clientY-this.mousePosition.y
+                
+                // update
+                this.tempItemPosition.x=n_x
+                this.tempItemPosition.y=n_y
 
-                target.style.transform = `translate(${this.position.translateX}px, ${this.position.translateY}px)`
+                const target = event.target as HTMLElement
+                Object.assign(target.style,{
+                    top:n_y,
+                    left:n_x
+                })
+
+                // execute hook
                 if (typeof props.draggableOptions?.onmove === 'function') {
                     props.draggableOptions.onmove(event)
                 }
             },
+
             onend: (event: DragEvent) => {
-                const n_x = this.position.translateX + this.position.x
-                const n_y = this.position.translateY + this.position.y
-                this.position.x = n_x
-                this.position.y = n_y
-
-                // update
-                event.target.style.left = n_x + 'px'
-                event.target.style.top = n_y + 'px'
-                event.target.style.transform = ''
-
-                // reset
-                this.position.translateX = 0
-                this.position.translateY = 0
-
+                this.setState({
+                    itemPosition:{
+                        x:this.tempItemPosition.x,
+                        y:this.tempItemPosition.y
+                    }
+                })
                 const target = event.target as HTMLElement
+                target.classList.remove('dragging')
                 if(!target.classList.contains('selected')){
                     target.classList.add('selected')
                 }
 
-            }
+            },
+            modifiers:[interact.modifiers.restrictRect({
+                restriction:'parent',
+                endOnly:true
+            })]
         }
         this.resizableOptions = {
             margin: 4,
             edges: Object.assign({ bottom: true, right: true, top: true, left: true }, props.resizableOptions?.edges),
             invert: props.resizableOptions?.invert || 'reposition',
+            onstart:(event:ResizeEvent)=>{
+                const target = event.target as HTMLElement
+                if(!target.classList.contains('selected')){
+                    target.classList.add('selected')
+                }
+            },
             onmove: (event: ResizeEvent) => {
-                console.log(111111)
-
-                console.log(event)
+                
                 if (typeof props.resizableOptions?.onmove === 'function') {
                     props.resizableOptions.onmove(event)
                 }
-            }
+                // update
+                const updateItemPosition={
+                    x:this.state.itemPosition.x,
+                    y:this.state.itemPosition.y
+                }
+                if(event.edges?.left){
+                    updateItemPosition.x+=event.delta.x
+                }
+                if(event.edges?.top){
+                    updateItemPosition.y+=event.delta.y
+                }
+
+                const updateItemSize={
+                    width:event.rect.width,
+                    height:event.rect.height
+                }
+
+                // this.setState({
+                //     itemPosition:updateItemPosition,
+                //     itemSize:updateItemSize
+                // })
+               
+            },
+            modifiers:[interact.modifiers.restrictSize({
+                min:{width:4,height:4}
+            })]
 
         }
-
-
     }
 
     setInteractions() {
         if (this.props.draggable) {
             this.interact?.draggable(this.draggableOptions)
         }
-
-        // if (this.props.dropzone){
-        //     this.interact?.dropzone(this.dropzoneOptions)
-        // } 
         if (this.props.resizable){
             this.interact?.resizable(this.resizableOptions)
         }
           
         this.interact?.on('tap',(event:PointerEvent)=>{
             const target = event.target as HTMLElement
-            target.classList.add('selected')
-            
+            if(!target.classList.contains('selected')){
+                target.classList.add('selected')
+            }
+          
         })
-     
     }
 
     componentDidMount() {
@@ -117,13 +184,17 @@ export class InteractElement extends React.Component<InteractProps>{
     }
 
     render(): React.ReactNode {
-
         return (
-
-            <div ref={(node: HTMLDivElement) => this.node = node} className='interact-item'>
+            <div ref={(node: HTMLDivElement) => this.node = node} style={
+                {
+                    top:this.state.itemPosition.y+'px',
+                    left:this.state.itemPosition.x+'px',
+                    width:this.state.itemSize.width+'px',
+                    height:this.state.itemSize.height+'px'
+                }
+            } className='interact-item'>
                 {this.props.children}
             </div>
-
         )
 
     }
